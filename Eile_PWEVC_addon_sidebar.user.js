2eb3380de9c2f0c8aac346c9745bb902fbbdd0f8
Eiledon <github@localhero.org.uk>
2015-08-10 15:50:19 +0100

    Complete rewrite of plug-in
    Rewrite of plug in, now has locally stored settings to control which
    games are visible on the panel

--- Eile_PWEVC_addon_sidebar.user.js
+++ Eile_PWEVC_addon_sidebar.user.js
@@ -1,85 +1,136 @@
 // ==UserScript==
-// @name        PWEVC Game Logo Sidebar
+// @name        PWE Game Panel
 // @namespace   https://github.com/Eiledon/PWEVC/
-// @downloadURL https://cdn.rawgit.com/Eiledon/PWEVC/master/Eile_PWEVC_addon_sidebar.user.js
-// @updateURL  https://cdn.rawgit.com/Eiledon/PWEVC/master/Eile_PWEVC_addon_sidebar.user.js
-// @version    0.3.1
-// @description  Adds sidebar with game logos in popup window linking to individual categories on pwe vanilla forums, game home and news page.
+// @downloadURL https://github.com/Eiledon/PWEVC/raw/master/Eile_PWEVC_addon_sidebar.user.js
+// @updateURL  https://github.com/Eiledon/PWEVC/raw/master/Eile_PWEVC_addon_sidebar.user.js
+// @version    1
+// @description  Adds popup window for each game on with links to game home page, news page and game forum. Users can now select which games to show and these settings are stored locally. 
 // @match      http://perfectworld.vanillaforums.com/*
 // @grant       none
 // @copyright  2015, Eiledon
 // ==/UserScript==
 
-
+var gameselection = "";
 
 // make tiles look nice
-function toTitleCase(str)
-{
+function toTitleCase(str) {
     return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-//get external css
-getCSS = function(url) {
-	var _head  = document.getElementsByTagName('head')[0];
-	var _link  = document.createElement('link');
-	_link.setAttribute('rel',"stylesheet");
-	_link.setAttribute('type',"text/css");
-	_link.setAttribute('media',"all");
-	_link.setAttribute('href',url);
-	_head.appendChild(_link);
 };
 
-getCSS("https://rawgit.com/Eiledon/PWEVC/master/Eile_PWEVC_gamepanel.css");
+//get external css
+function addCSS(url) {
+    var _head  = document.getElementsByTagName('head')[0];
+    var _link  = document.createElement('link');
+    _link.setAttribute('rel',"stylesheet");
+    _link.setAttribute('type',"text/css");
+    _link.setAttribute('media',"all");
+    _link.setAttribute('href',url);
+    _head.appendChild(_link);
+};
 
-// initialise main panel elements
-var _gameouter = $("<div id='floatnav'></div>");
-var _gameinner = $("<div class='floatbutton'></div>");
-var _gamebutton = $("<img src='http://www.arcgames.com/images/download/logo.png' width='30' alt='Games Menu' title='Games Menu' />");
-var _gamepopout = $("<div class='popoutpanel'></div>");
-var _gamelist = $("<ul></ul>");
+function makePanel() {
+    $('#gpouter').remove(); //remove any previous iteration
+    // panel logo/activation button
+    var $gplogo = $("<img src='http://www.arcgames.com/images/download/logo.png' width='30' alt='Games Menu' title='Games Menu' />");
+    //define generic parts
+    var _container = '<div></div>';
+    var _list = '<ul></ul>';
+    var _item = '<li></li>';
+    var _link = '<a></a>'
+    //var _select = '<select multiple></select>';
+    //var _option = '<option></option>';
+    var _check = '<input type="checkbox">'; //id, name, value
+    var _label = '<label></label>'; //for
 
-//permanent ARC additions
-var _li = $('<li class="gameitem"></li>');
+
+    //define main containers/controls
+    var $gpouter = $(_container).attr('id','gpouter');
+    var $gpbutton =$(_container).attr('id', 'gpbutton').append($gplogo);
+    var $gppop = $(_container).attr('id','gppop');
+    var $gpgamelist = $(_list).attr('id','gpgamelist');
+    var $gpselect = $(_list).attr('id','gpselect');
+    var $gpopt = $(_check).attr('name','check').attr('id','gpopt');
+    var $gpoptbutton = $(_label).attr('id','gpoptbutton').attr('for','gpopt').text('Select Games').click(toggleGames);
+
+
+    //always present arc details
+    //create section containers
+    var $gpgameitem = $(_item).attr('id','gp_arc').addClass('gpgameitem').css('background-image', 'url(https://raw.githubusercontent.com/Eiledon/PWEVC/master/arclogo.png)');
+    var $gplinklist = $(_list).addClass('gplinklist');
+    //generate links
+    $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href','https://www.arcgames.com/en/my/account').attr('target','_blank').attr('alt','ARC Profile').attr('title','ARC Profile').text('ACCOUNT')));
+    $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href','https://billing.arcgames.com/en/').attr('target','_blank').attr('alt','Charge Zen').attr('title','Charge Zen').text('CHARGE')));
+    $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href','https://support.arcgames.com/app/home').attr('target','_blank').attr('alt','ARC Support').attr('title','ARC Support').text('SUPPORT')));
+    //append into games list container
+    $gpgamelist.append($gpgameitem.append($gplinklist))
+
+    // open and loop through games array
+    $.getJSON('https://rawgit.com/Eiledon/PWEVC/master/games.json', function(json) {
+        $.each(json.games, function(_index, _element) {
+            //should entry be visible?
+            if (gameselection.indexOf(_element.catname) >= 0) { var _visible = true;} else { var _visible = false;}
+
+            //collect/define game specific variables
+            var _propername = toTitleCase(_element.arcname.replace(/(-|_)/g," ")); 
+            var _arcurl = 'http://www.arcgames.com/en/games/'+ _element.arcname; 
+            var _caturl = 'http://perfectworld.vanillaforums.com/categories/'+_element.catname; 
+            var _imgurl = _element.logo; 
+
+
+            //add option to select list
+            $gpselect.append($(_item).append($(_check).attr('name','check').attr('value','#gp_' + _element.catname).attr('id','gps_' + _element.catname).prop( "checked", _visible )).append($(_label).attr('for','gps_' + _element.catname).text(_propername)));
+
+            //create section containers
+            var $gpgameitem = $(_item).attr('id','gp_' + _element.catname).addClass('gpgameitem').css('background-image', 'url(' + _imgurl + ')');
+
+            if (_visible == false ) { $gpgameitem.hide(); }
+
+            var $gplinklist = $(_list).addClass('gplinklist');
+            //generate links
+            $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href',_arcurl).attr('target','_blank').attr('alt',_propername + ' Game Homepage').attr('title',_propername + ' Game Homepage').text('HOME')));
+            $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href',_arcurl + '/news').attr('target','_blank').attr('alt',_propername + ' Game News').attr('title',_propername + ' Game News').text('NEWS')));
+            $gplinklist.append($(_item).addClass('gplinkitem').append($(_link).addClass('gplink').attr('href',_caturl).attr('alt',_propername + ' Game Forum').attr('title',_propername + ' Game Forum').text('FORUM'))) ;
+            //append into games list container
+            $gpgamelist.append($gpgameitem.append($gplinklist)); 
+        });
+
+    })
+    .complete(function(){
+        //on completion of json load append all generated content into page
+        $gpouter.append($gpbutton.append($gppop.append($gpgamelist).append($gpopt).append($gpoptbutton).append($gpselect)));
+        $gpouter.appendTo('#Frame');
+    }); 
+}
+
+function toggleGames() {
+    var _selected = $( "#gpselect input:checkbox:checked" ).map(function(){ return this.value }).get().join(", ");
+    var _unselected = $( "#gpselect input:checkbox:not(:checked)").map(function(){ return  this.value }).get().join(", ");
     
-//combine each game into main list
-_li.append($('<div class="gamebox"><img width="140" title="ARC" alt="ARC" src="http://images-cdn.perfectworld.com/arcportal_static/images/global/logos.png"></div>'));
-_li.append($('<div class="gamelinks"><a href="https://www.arcgames.com/en/my/account" target="_blank" alt="ARC Profile" title="ARC Profile">ACCOUNT</a>&nbsp;|&nbsp;<a href="https://billing.arcgames.com/en/" target="_blank"  alt="Charge Zen" title="Charge Zen">ZEN</a>&nbsp;|&nbsp;<a href="https://support.arcgames.com/app/home" target="_blank"  alt="ARC Support" title="ARC Support">SUPPORT</a></div>')); 
-_gamelist.append(_li);
+    if($('#gpopt:checked').length) { 
+        $(_selected).show(200);
+        $(_unselected).hide(200);
+        gameselection = _selected; 
+        saveSettings(); ; 
+    }
+}
+
+// amdended settings names
+function saveSettings(){
+    localStorage["pweGamePanel"] =  gameselection;
+    //console.log('saved: ' + gameselection);
+};
+
+//amended settings names
+function getSettings() {
+    var savedselection = localStorage["pweGamePanel"];
+    if (savedselection) { gameselection = savedselection; }
+    //console.log('loaded: ' + gameselection);
+};
 
 
-
-// open and loop through games array to populate inner container with list elements
-$.getJSON('https://rawgit.com/Eiledon/PWEVC/master/games.json', function(json) {
-  $.each(json.games, function(_index, _element) {
-
-    // extract game specific data
-    var _catname = _element.catname;
-    var _caturl = 'http://perfectworld.vanillaforums.com/categories/'+_catname;
-    
-    var _arcname = _element.arcname;
-    var _arcurl = 'http://www.arcgames.com/en/games/'+_arcname;
-    
-    // format arc name into nice looking title  
-    var _propername = toTitleCase(_arcname.replace(/(-|_)/g," ")); 
-
-    var _imgurl = _element.logo; 
-  
-    var _li = $('<li class="gameitem"></li>');
-    
-      
-      http://www.arcgames.com/en/games/star-trek-online/news
-      
-      
-    //combine each game into main list
-    _li.append($('<div class="gamebox"><img width="150" title="'+_propername+'" alt="'+_propername+'" src="'+_imgurl+'"></div>'));
-    _li.append($('<div class="gamelinks"><a href="'+_arcurl+'" target="_blank" title="Visit Game Homepage" alt="Visit Game Homepage">HOME</a>&nbsp;|&nbsp;<a href="'+_arcurl+'/news" target="_blank" title="See Game News" alt="See Game News">NEWS</a>&nbsp;|&nbsp;<a href="'+_caturl+'"  title="Visit Game Forum" alt="Visit Game Forum">FORUM</a></div>')); 
-    _gamelist.append(_li);
-  });
-});
-
-// combine panel elements and add to page
-_gamepopout.append(_gamelist);
-_gameinner.append(_gamebutton);
-_gameinner.append(_gamepopout);
-_gameouter.append(_gameinner);
-$("#Frame").append(_gameouter);
+$( document ).ready(function() {
+    //initialise plug in
+    addCSS("https://rawgit.com/Eiledon/PWEVC/master/PWE_Game_Panel.css");
+    getSettings();
+    makePanel();
+}); 
